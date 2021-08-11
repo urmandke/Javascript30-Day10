faf4fdb450c08ac8346d6fb0c0739e4478f1387b
Udayraj <udayraj.m@bizamica.com>
2021-08-11 07:11:24 +0530

    feature of holding down shift and checks inbetween checkboxes is complete

--- script.js
+++ script.js
@@ -3,30 +3,28 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 checkboxes.forEach(checkbox => checkbox.addEventListener('click',handleCheck));
 
 let lastChecked;
-let inBetween = false;
+
 
 function handleCheck(e){
-    //Check if they had the shift key down
-    //And check if they are checking it
-    if(e.shiftKey && this.checked){
-    //loop over every single checkbox 
-    checkboxes.forEach(checkbox =>{
-        console.log(checkbox); 
-        if(checkbox == this || lastChecked){
-            inBetween = !inBetween;
-            console.log('Starting to check inbetween')
-        }
-        
-        if(inBetween){
-            checkbox.checked = true;
-        }
+// Check if they had the shift key down
+  // AND check that they are checking it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('Starting to check them in between!');
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
     });
+  }
 
-   
-        
-    }
-
-    console.log(e);
-    lastChecked = this;
+  lastChecked = this;
+  
 }
 
